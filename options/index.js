189b49edf00d873c61e94a4485cdb1b8c8746190
Kactus83 <93870523+Kactus83@users.noreply.github.com>
2023-03-14 13:38:37 +0100

    UPGRADE CSS & Display logic for options

--- options/index.js
+++ options/index.js
@@ -1,20 +1,37 @@
 const apiKeyInput = document.getElementById("api-key");
+const saveBtn = document.getElementById("save-btn");
+const editBtn = document.getElementById("edit-btn");
 
 function saveApiKey() {
   const apiKey = apiKeyInput.value.trim();
 
   if (!apiKey) {
-    return alert("Veuillez entrer une clé API valide.");
+    return alert("Please enter a valid API key.");
   }
 
   chrome.storage.local.set({ apiKey }, () => {
-    alert("Clé API enregistrée avec succès !");
+    apiKeyInput.classList.add("disabled");
+    saveBtn.classList.add("hidden");
+    editBtn.classList.remove("hidden");
+    alert("API Key saved successfully!");
   });
 }
 
 function loadApiKey() {
   chrome.storage.local.get("apiKey", (result) => {
-    apiKeyInput.value = result.apiKey || "";
+    const apiKey = result.apiKey || "";
+
+    apiKeyInput.value = apiKey;
+
+    if (apiKey) {
+      apiKeyInput.classList.add("disabled");
+      saveBtn.classList.add("hidden");
+      editBtn.classList.remove("hidden");
+    } else {
+      apiKeyInput.classList.remove("disabled");
+      saveBtn.classList.remove("hidden");
+      editBtn.classList.add("hidden");
+    }
   });
 }
 
@@ -23,4 +40,10 @@ loadApiKey();
 document.getElementById("options-form").addEventListener("submit", (e) => {
   e.preventDefault();
   saveApiKey();
+});
+
+editBtn.addEventListener("click", () => {
+  apiKeyInput.classList.remove("disabled");
+  saveBtn.classList.remove("hidden");
+  editBtn.classList.add("hidden");
 });
