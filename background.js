1f46ab1076c308b4d0efdf5355f771b2e13574f5
Kactus83 <93870523+Kactus83@users.noreply.github.com>
2023-03-14 10:14:57 +0100

    REFACTOR / DEBUGING :
     - supr modules (don't know how to use this kind of architecture with google extension)
     - add all in one pattern with background and content-script

--- background.js
+++ background.js
@@ -1,21 +1,105 @@
-// Crée un menu contextuel avec une option "first-option"
+// Texte selectionné actualisé en temps réel
+var selectedText = '';
+// Variables pour les requettes
+let request1 = '';
+let request2 = '';
+let request3 = '';
+let request4 = '';
+
+// Crée les menus
 chrome.contextMenus.create({
-  "id": "first-option",
-  "title": "Option maison",
+  "id": "option-1",
+  "title": "Expliquer",
   "contexts": ["selection"],
   "enabled": false
 });
+
+chrome.contextMenus.create({
+  "id": "option-2",
+  "title": "Resumer",
+  "contexts": ["selection"],
+  "enabled": false
+});
+
+chrome.contextMenus.create({
+  "id": "option-3",
+  "title": "Resoudre",
+  "contexts": ["selection"],
+  "enabled": false
+});
+
+chrome.contextMenus.create({
+  "id": "option-4",
+  "title": "Repondre",
+  "contexts": ["selection"],
+  "enabled": false
+});
+
+
 
 // Écoute les messages envoyés depuis le contenu
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.selectedText) {
     // Vérifie la longueur de la sélection
     if (request.selectedText.length >= 2) {
-      // Active l'option du menu contextuel
-      chrome.contextMenus.update("first-option", {"enabled": true});
+        // Stocke le texte sélectionné dans la variable globale
+        selectedText = selectedText;
+        // Actualisation des requettes
+        request1 = request.selectedText;
+        request2 = request.selectedText;
+        request3 = request.selectedText;
+        request4 = request.selectedText;
+        // Active l'option du menu contextuel
+        chrome.contextMenus.update("option-1", {"enabled": true});
+        chrome.contextMenus.update("option-2", {"enabled": true});
+        chrome.contextMenus.update("option-3", {"enabled": true});
+        chrome.contextMenus.update("option-4", {"enabled": true});
     } else {
-      // Désactive l'option du menu contextuel
-      chrome.contextMenus.update("first-option", {"enabled": false});
+        // Reset les requettes
+        request1 = '';
+        request2 = '';
+        request3 = '';
+        request4 = '';
+        // Réinitialise la variable globale si aucun texte n'est sélectionné
+        selectedText = '';
+        // Désactive l'option du menu contextuel
+        chrome.contextMenus.update("option-1", {"enabled": false});
+        chrome.contextMenus.update("option-2", {"enabled": false});
+        chrome.contextMenus.update("option-3", {"enabled": false});
+        chrome.contextMenus.update("option-4", {"enabled": false});
     }
+  }
+});
+
+// Fonction pour réagir au clic sur l'option "GPT - Expliquer"
+function onOption1Click(info, tab) {
+  alert("Requête 1 : " + request1);
+}
+
+// Fonction pour réagir au clic sur l'option "GPT - Résumer"
+function onOption2Click(info, tab) {
+  alert("Requête 2 : " + request2);
+}
+
+// Fonction pour réagir au clic sur l'option "GPT - Résoudre"
+function onOption3Click(info, tab) {
+  alert("Requête 3 : " + request3);
+}
+
+// Fonction pour réagir au clic sur l'option "GPT - Répondre"
+function onOption4Click(info, tab) {
+  alert("Requête 4 : " + request4);
+}
+
+// Ajout des écouteurs d'événements pour les clics sur les options
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId === "option-1") {
+    chrome.tabs.sendMessage(tab.id, {option: "explain", request: request1});
+  } else if (info.menuItemId === "option-2") {
+    chrome.tabs.sendMessage(tab.id, {option: "summarize", request: request2});
+  } else if (info.menuItemId === "option-3") {
+    chrome.tabs.sendMessage(tab.id, {option: "solve", request: request3});
+  } else if (info.menuItemId === "option-4") {
+    chrome.tabs.sendMessage(tab.id, {option: "answer", request: request4});
   }
 });
--- content-script.js
+++ content-script.js
@@ -17,3 +17,19 @@ document.addEventListener('touchend', function(event) {
   // Envoie un message à background.js avec le texte sélectionné ou une chaîne de caractères vide s'il n'y en a pas
   chrome.runtime.sendMessage({selectedText: selectedText || ''});
 });
+
+// Ecoute les resultats de requettes
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.option === "explain") {
+    alert("Explication : " + request.request);
+  }
+  if (request.option === "summarize") {
+    alert("Resume : " + request.request);
+  }
+  if (request.option === "solve") {
+    alert("Resolution : " + request.request);
+  }
+  if (request.option === "answer") {
+    alert("Reponse : " + request.request);
+  }
+});
