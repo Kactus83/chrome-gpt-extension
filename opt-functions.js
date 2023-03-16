5e7946643b158494e227b618284685d4b8f4069a
Kactus83 <93870523+Kactus83@users.noreply.github.com>
2023-03-16 10:30:58 +0100

    WIP Handle stream in background

--- opt-functions.js
+++ opt-functions.js
@@ -2,17 +2,41 @@
 import { sendOpenAIRequest } from "./requests/api-request.js";
 import { createRequest } from "./requests/request-factory.js";
 
+// Fonction générique pour traiter les données reçues en streaming
+async function handleStream(streamPromise, tabId) {
+  let result = '';
+  const stream = await streamPromise;
+
+  const reader = stream.body.getReader();
+
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) break;
+    result += new TextDecoder().decode(value);
+    const completion = parseCompletion(result);
+    if (completion !== null) {
+      chrome.runtime.sendMessage({ option: "stream", response: completion }, (response) => {
+        // Callback pour gérer la réponse
+        console.log(response);
+      });
+      result = '';
+    }
+  }
+}
+
+
 // Fonction pour réagir au clic sur l'option "GPT - Expliquer"
 export async function onOption1Click(info, tab, selectedText, apiKey, aiVersion, language, apiAddress) {
   try {
     // Création de la requête
     const request = await createRequest(1, selectedText, language, aiVersion);
 
-    // Envoi de la requête à OpenAI
-    const response = await sendOpenAIRequest(apiKey, apiAddress, request);
+    // Envoi de la requête à OpenAI avec un stream de réponse
+    const streamPromise = sendOpenAIRequestWithStream(apiKey, apiAddress, request);
 
-    console.log(response);
-    chrome.tabs.sendMessage(tab.id, {option: "explain", response: response});
+    // Traiter les données en streaming
+    handleStream(streamPromise, tab.id);
+
   } catch (error) {
     // Envoyer un message au content script indiquant qu'il y a une erreur
     let errorData = {
@@ -21,23 +45,23 @@ export async function onOption1Click(info, tab, selectedText, apiKey, aiVersion,
       type: null
     };
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {error: true, data: errorData});
+      chrome.tabs.sendMessage(tabs[0].id, {error: true, data: errorData.message});
     });
     console.error(error);
   }
 }
-  
+
 // Fonction pour réagir au clic sur l'option "GPT - Résumer"
 export async function onOption2Click(info, tab, selectedText, apiKey, aiVersion, language, apiAddress) {
   try {
     // Création de la requête
     const request = await createRequest(2, selectedText, language, aiVersion);
 
-    // Envoi de la requête à OpenAI
-    const response = await sendOpenAIRequest(apiKey, apiAddress, request);
+    // Envoi de la requête à OpenAI avec un stream de réponse
+    const streamPromise = sendOpenAIRequestWithStream(apiKey, apiAddress, request);
 
-    console.log(response);
-    chrome.tabs.sendMessage(tab.id, {option: "summarize", response: response});
+    // Gestion des données en streaming
+    handleStream(streamPromise, tab.id, "summarize");
   } catch (error) {
     // Envoyer un message au content script indiquant qu'il y a une erreur
     let errorData = {
@@ -52,17 +76,17 @@ export async function onOption2Click(info, tab, selectedText, apiKey, aiVersion,
   }
 }
 
-      
 // Fonction pour réagir au clic sur l'option "GPT - Résoudre"
 export async function onOption3Click(info, tab, selectedText, apiKey, aiVersion, language, apiAddress) {
   try {
     // Création de la requête
     const request = await createRequest(3, selectedText, language, aiVersion);
 
-    // Envoi de la requête à OpenAI
-    const response = await sendOpenAIRequest(apiKey, apiAddress, request);
-    console.log(response);
-    chrome.tabs.sendMessage(tab.id, {option: "solve", response: response});
+    // Envoi de la requête à OpenAI avec un stream de réponse
+    const streamPromise = sendOpenAIRequestWithStream(apiKey, apiAddress, request);
+
+    // Gestion des données en streaming
+    handleStream(streamPromise, tab.id, "solve");
   } catch (error) {
     // Envoyer un message au content script indiquant qu'il y a une erreur
     let errorData = {
@@ -83,10 +107,11 @@ export async function onOption4Click(info, tab, selectedText, apiKey, aiVersion,
     // Création de la requête
     const request = await createRequest(4, selectedText, language, aiVersion);
 
-    // Envoi de la requête à OpenAI
-    const response = await sendOpenAIRequest(apiKey, apiAddress, request);
-    console.log(response);
-    chrome.tabs.sendMessage(tab.id, {option: "answer", response: response});
+    // Envoi de la requête à OpenAI avec un stream de réponse
+    const streamPromise = sendOpenAIRequestWithStream(apiKey, apiAddress, request);
+
+    // Gestion des données en streaming
+    handleStream(streamPromise, tab.id, "answer");
   } catch (error) {
     // Envoyer un message au content script indiquant qu'il y a une erreur
     let errorData = {
@@ -100,3 +125,4 @@ export async function onOption4Click(info, tab, selectedText, apiKey, aiVersion,
     console.error(error);
   }
 }
+
